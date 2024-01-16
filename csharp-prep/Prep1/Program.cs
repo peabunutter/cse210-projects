d43546589fdc7012c2cfd5204d53eec9802b905d
Peabunutter <theporterbello@gmail.com>
2024-01-15 21:04:00 -0800

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"My name is {last}, {first} {last}.");
     }
 }
