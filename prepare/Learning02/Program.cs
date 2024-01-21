c3201de587c7f62d7bd01b7e58b2f871645d4d9b
Peabunutter <theporterbello@gmail.com>
2024-01-20 17:36:30 -0800

    week 2 lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+Job job1 = new()
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
-}
+    _company = "Microsoft",
+    _jobTitle = "Software Engineer",
+    _startYear = 2000,
+    _endYear = 2020
+};
+Job job2 = new()
+{
+    _company = "Apple",
+    _jobTitle = "Head of Operations",
+    _startYear = 1987,
+    _endYear = 2019
+};
+Resume myResume = new()
+{
+    _name = "Jimothy Anderson"
+};
+
+myResume._jobs.Add(job1);
+myResume._jobs.Add(job2);
+
+myResume.Display();
